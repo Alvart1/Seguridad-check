bd09d122616aa21a14e29d181f26d40f9fb49873
Alvart1 <alvaritoromerovillegas@gmail.com>
2026-02-24 10:42:40 +0100

    pn

--- js/panel-propietario.js
+++ js/panel-propietario.js
@@ -1,60 +1,26 @@
-const URL_SUPA = 'https://xfwovtrlpipnghoyduql.supabase.co';
-const KEY_SUPA = 'sb_publishable_xi9wcDolJG6kKTnU_2O0fA_n507M8fu'; 
-const supabase = supabase.createClient(URL_SUPA, KEY_SUPA);
-
-function mostrarSeccion(id) {
-    document.querySelectorAll('.seccion')
-        .forEach(sec => sec.style.display = 'none');
-
-    document.getElementById(id).style.display = 'block';
-
-    if (id === 'reservas') {
-        cargarReservas();
-    }
+function irCamaras() {
+    window.location.href = "http://IP_RASPBERRY/zm";
 }
 
-async function cargarReservas() {
+function irReservas() {
+    window.location.href = "reservas.html";
+}
 
-    const { data, error } = await supabase
-        .from('reservas')
-        .select(`
-            id,
-            fecha_entrada,
-            hospedes(nombre, apellidos, codigo_documento)
-        `);
-
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-    const contenedor = document.getElementById('lista-reservas');
-    contenedor.innerHTML = '';
-
-    data.forEach(r => {
-        const div = document.createElement('div');
-        div.style.border = "1px solid #ccc";
-        div.style.margin = "10px";
-        div.style.padding = "10px";
-
-        div.innerHTML = `
-            <h3>Reserva ${r.id}</h3>
-            <p>Fecha: ${r.fecha_entrada}</p>
-            ${r.hospedes.map(h =>
-                `<p>${h.nombre} ${h.apellidos} - ${h.codigo_documento}</p>`
-            ).join('')}
-        `;
-
-        contenedor.appendChild(div);
-    });
+function irLlaves() {
+    window.location.href = "acceso-llaves.html";
 }
 
 async function abrirCajon() {
 
-    await fetch("http://IP_RASPBERRY:3000/abrir-cajon", {
-        method: "POST"
-    });
+    try {
+        await fetch("http://IP_RASPBERRY:3000/abrir-cajon", {
+            method: "POST"
+        });
 
-    alert("Cajón abierto");
+        alert("Cajón abierto correctamente");
+
+    } catch (error) {
+        alert("Error al abrir el cajón");
+        console.error(error);
+    }
 }
-
